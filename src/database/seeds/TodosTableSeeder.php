ac453abac0400069b5d633d3ce1c5cae12f87bd2
Sora9833 <sorafluteit@gmail.com>
2024-01-16 20:29:17 +0900

    add: todosテーブル用のマイグレーションファイル作成。テストデータ挿入用のseederファイルを作成。

---
+++ src/database/seeds/TodosTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 以下追加
+        DB::table('todos')->insert([
+            [
+                'content' => '開発環境を構築する',
+                'is_completed' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'content' => 'Laravelをインストールする',
+                'is_completed' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
