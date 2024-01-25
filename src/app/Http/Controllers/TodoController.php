06439e31220c7704f88baae2867e49216292802c
Sora9833 <sorafluteit@gmail.com>
2024-01-25 13:54:34 +0900

    Requests/TodoRequest.phpを作成し、バリエーションルールを設定。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Todo;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
@@ -25,7 +26,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -45,13 +46,12 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
         $todo->fill($inputs);
         $todo->save();
-        dd($this->todo->id, $todo->id);
         return redirect()->route('todo.show', $todo->id);
     }
 }
